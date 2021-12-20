b3051c41df8c45bc0977fbf3f90b32999226b0eb
AhmedRabea0302 <arabea169@gmail.com>
2021-12-20 18:40:09 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+
+  <teleport to=".modals" v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Signup Now</a>
+        <a href="#">More Info</a>
+      </template>
+      <h1>Athletes Never Surrender</h1>
+      <p>Choose Youe Equipment from a big stock</p>
+    </Modal>
+  </teleport>
+
+  <teleport to=".modals" v-if="showModal2">
+    <Modal @close="toggleModal2">
+      <h1>Sand Bag</h1>
+      <p>Sand Bag that weights 45 KG</p>
+
+      <template v-slot:links>
+        <a href="#">Love Product</a>
+        <a href="#">More Info</a>
+      </template>
+    </Modal>
+  </teleport>
+
+  <p>Welcome ...</p>
+
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModal2">Open Sand Bag Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from './components/Modal.vue'
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    Modal,
+  },
+
+  data() {
+    return {
+      title: 'Hello from the other side',
+      header: 'Hello From props',
+      text: 'paragarph text',
+      showModal: false,
+      showModal2: false
+    }
+  }, 
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+      console.log(this.showModal)
+    },
+
+    toggleModal2() {
+      this.showModal2 = !this.showModal2;
+    }
+
   }
+  
 }
 </script>
 
@@ -22,5 +71,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.active {
+  border: 2px solid green;
 }
 </style>
